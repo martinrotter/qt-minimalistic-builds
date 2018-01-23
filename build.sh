2f8f358a173b05af1ae7e5281c897c20dbcdd82e
Martin Rotter <rotter@praktik.cz>
2018-01-23 13:20:11 +0100

    Advancements. Working dynamic build including deployment.

--- build.sh
+++ build.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
 
-cd <PKGBUILD-ROOT>
+VERSION="5.10"
+TYPE="dynamic"
+TARGET_FOLDER="qt-${VERSION}-${TYPE}-mingw-w64-x86_64"
+
+# Build initial dynamic package.
+cd "$VERSION"
 makepkg -sL
+
+# Copy all package stuff from "pkg" subfolder.
+mkdir "$TARGET_FOLDER"
+mv ./pkg/mingw64/* "./$TARGET_FOLDER"
+
+# Copy all dependencies.
+echo "c:\\msys64\\mingw64\\" > "qt_paths.txt"
+
+# Generate lists of dependent DLLs.
+for f in $TARGET_FOLDER/bin/*.dll
+do
+  ../tools/FindDependencies.exe $f qt_paths.txt
+done
+
+# Copy dependent DLLs into single folder.
+for f in ./*/copydlls.bat
+do
+  $f
+done
+
+# Deploy dependent DLLs.
+cp ./bin/* "$TARGET_FOLDER" -v
+cp ../tools/qtbinpatcher.exe "$TARGET_FOLDER" -v
+
+# Create archive.
+../tools/7za a -t7z "$TARGET_FOLDER.7z" "./$TARGET_FOLDER" -mmt -mx9
