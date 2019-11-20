3e1ee6b84595d0f84e6be14dfea13ad97b1c7baa
Martin Rotter <rotter@praktik.cz>
2019-11-20 10:57:07 +0100

    qt5.12.6

---
+++ msvc-build5126-openssl111.ps1
@@ -0,0 +1,60 @@
+# 1. Start Visual Studio x64 Native Tools command line.
+# 2. Run powershell.exe from Native Tools cmd.
+# 3. cd to path of qt5-minimalistic-builds repo.
+
+$version_base = "5.12"
+$version = "5.12.6"
+
+$qt_sources_url = "https://download.qt.io/official_releases/qt/" + $version_base + "/" + $version + "/single/qt-everywhere-src-" + $version + ".zip"
+$qt_archive_file = $pwd.Path + "\qt-" + $version + ".zip"
+$qt_src_base_folder = $pwd.Path + "\qt-everywhere-src-" + $version
+
+$tools_folder = $pwd.Path + "\tools\"
+$type = "dynamic"
+$prefix_base_folder = "qt-" + $version + "-" + $type + "-msvc2017-x86_64"
+$prefix_folder = $pwd.Path + "\" + $prefix_base_folder
+$build_folder = $pwd.Path + "\qtbuild"
+
+# OpenSSL
+# 1.1.1d
+# Install steps:
+#   1. Download, unpack openssl to folder "C:\openssl111d".
+#   2. Install system-wide perl, nasm.
+#   3. Run MVSC command prompt and navigate to openssl folder.
+#   4. Run "perl Configure VC-WIN64A --prefix=c:\openssl111 --openssldir=C:\openssl111" and then run "nmake" and "make install".
+$openssl_base_folder = "C:\openssl111"
+$openssl_include_folder = $openssl_base_folder + "\include"
+$openssl_libs_folder = $openssl_base_folder + "\lib"
+$openssl_bin_folder = $openssl_base_folder + "\bin"
+
+# Download Qt sources, unpack.
+$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+
+Invoke-WebRequest -Uri $qt_sources_url -OutFile $qt_archive_file
+& "$tools_folder\7za.exe" x $qt_archive_file
+
+# Configure.
+mkdir $build_folder
+cd $build_folder
+
+& "$qt_src_base_folder\configure.bat" -debug-and-release -opensource -confirm-license -platform win32-msvc2017 -opengl desktop -no-iconv -no-dbus -no-icu -no-fontconfig -no-freetype -qt-harfbuzz -nomake examples -nomake tests -skip qt3d -skip qtactiveqt -skip qtcanvas3d -skip qtconnectivity -skip qtdeclarative -skip qtdatavis3d -skip qtdoc -skip qtgamepad -skip qtgraphicaleffects -skip qtlocation -skip qtmultimedia -skip qtnetworkauth -skip qtpurchasing -skip qtquickcontrols -skip qtquickcontrols2 -skip qtremoteobjects -skip qtscxml -skip qtsensors -skip qtserialbus -skip qtserialport -skip qtspeech -skip qtvirtualkeyboard -skip qtwebchannel -skip qtwebengine -skip qtwebsockets -skip qtwebview -skip qtscript -mp -optimize-size -shared -prefix $prefix_folder -openssl -openssl-linked -I $openssl_include_folder -L $openssl_libs_folder OPENSSL_LIBS="-lUser32 -lAdvapi32 -lGdi32 -llibcrypto -llibssl"
+
+# Compile.
+#& "$tools_folder\jom.exe"
+nmake
+nmake install
+
+# Copy qtbinpatcher, OpenSSL.
+cp "$tools_folder\qtbinpatcher.*" "$prefix_folder\bin\"
+cp "$openssl_bin_folder\*.dll" "$prefix_folder\bin\"
+cp "$openssl_bin_folder\*.exe" "$prefix_folder\bin\"
+cp "$openssl_bin_folder\*.pdb" "$prefix_folder\bin\"
+cp "$openssl_libs_folder\*" "$prefix_folder\lib\" -Recurse
+cp "$openssl_include_folder\openssl" "$prefix_folder\include\" -Recurse
+
+# Fixup OpenSSL DLL paths.
+gci -r -include "*.prl" $prefix_folder | foreach-object { $a = $_.fullname; ( get-content $a ) | foreach-object { $_ -replace "C:\\\\openssl111\\\\lib", '$$$$[QT_INSTALL_LIBS]\\' } | set-content $a }
+
+# Create final archive.
+& "$tools_folder\7za.exe" a -t7z "${prefix_base_folder}-openssl111a.7z" "$prefix_folder" -mmt -mx9
